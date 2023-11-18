f17b1a209a65d73b41a933477a972045fd578a2c
jorisj <jorisjolles@gmail.com>
2023-11-18 22:22:56 +0100

    chore: added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,17 @@
-import { createSignal } from 'solid-js'
-import solidLogo from './assets/solid.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import imgUrl from './assets/283831035-46846c93-9d66-40a6-9431-7c25bd1c8dd0.png'
 
 function App() {
-  const [count, setCount] = createSignal(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={solidLogo} class="logo solid" alt="Solid logo" />
-        </a>
-      </div>
-      <h1>Vite + Solid</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count()}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Solid logos to learn more
-      </p>
-    </>
+    <div class='container mx-auto px-8 flex justify-center'>
+      <h1>
+        <span class='sr-only'>
+          GO CLEAN
+        </span>
+        <img src={imgUrl} class="object-cover" alt="" />
+      </h1>
+    </div>
   )
 }
 
